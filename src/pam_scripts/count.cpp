2736050ca9c3fc0153128badd50b81e50534a2fc
cm43 <cm43@sanger.ac.uk>
2025-10-20 14:01:50 +0100

    Concurrent queue

--- src/pam_scripts/count.cpp
+++ src/pam_scripts/count.cpp
@@ -4,11 +4,14 @@
 #include <cstring>
 #include <vector>
 #include <zlib.h>
+#include <thread>
+#include <atomic>
 #include "kseq.h"
+#include "concurrentqueue.h"
 
 KSEQ_INIT(gzFile, gzread)
 
-uint8_t encode_base(uint8_t base)
+static inline uint8_t encode_base(uint8_t base)
 {
     switch (base)
     {
@@ -29,7 +32,8 @@ uint8_t encode_base(uint8_t base)
     }
 }
 
-std::vector<uint64_t> extract_kmers_destructive(kstring_t *seq, uint8_t k)
+static inline std::vector<uint64_t> extract_kmers_destructive(
+    kstring_t *seq, uint8_t k)
 {
     std::vector<uint64_t> kmers;
     size_t l = seq->l;
@@ -69,10 +73,26 @@ std::vector<uint64_t> extract_kmers_destructive(kstring_t *seq, uint8_t k)
     return kmers;
 }
 
-inline int stage_bucket(kseq_t *ks, std::vector<kstring_t> buckets, size_t count)
+static inline void worker_func(
+    std::vector<kstring_t> &buckets,
+    moodycamel::ConcurrentQueue<size_t> &filled_buckets,
+    moodycamel::ConcurrentQueue<size_t> &free_buckets,
+    std::atomic<bool> &done,
+    uint8_t k)
 {
-    ks->seq = buckets[count % 128];
-    return 1;
+    size_t i;
+    while (true)
+    {
+        if (filled_buckets.try_dequeue(i))
+        {
+            std::vector<uint64_t> kmers = extract_kmers_destructive(&buckets[i], k);
+            free_buckets.enqueue(i);
+        }
+        else if (done.load())
+            break; // no more work coming
+        else
+            std::this_thread::yield(); // wait for new items
+    }
 }
 
 int main(int argc, char **argv)
@@ -88,56 +108,56 @@ int main(int argc, char **argv)
     if (!fp)
         return 1;
 
-    std::vector<kstring_t> buckets(128, kstring_t{0, 0, nullptr});
+    const size_t num_buckets = 128;
+    const size_t num_threads = std::thread::hardware_concurrency();
+    const uint8_t k = 31;
+    std::fprintf(stderr, "Total threads: %zu\n", num_threads);
+
+    std::vector<kstring_t> buckets(num_buckets, kstring_t{0, 0, nullptr});
+    moodycamel::ConcurrentQueue<size_t> free_buckets;
+    moodycamel::ConcurrentQueue<size_t> filled_buckets;
+    std::atomic<bool> done(false);
+
+    for (size_t i = 0; i < num_buckets; i++)
+        free_buckets.enqueue(i);
+
+    std::vector<std::thread> workers;
+    for (size_t t = 0; t < num_threads; t++)
+        workers.emplace_back(worker_func,
+                             std::ref(buckets),
+                             std::ref(filled_buckets),
+                             std::ref(free_buckets),
+                             std::ref(done),
+                             k);
 
     kseq_t *ks = kseq_init(fp);
     size_t count = 0;
     while (1)
     {
-        size_t i = count % 128;
+        size_t i;
+        if (!free_buckets.try_dequeue(i)) // No free bucket available, wait
+        {
+            std::this_thread::yield();
+            continue;
+        }
         ks->seq = buckets[i];
         if (kseq_read(ks) < 0)
             break;
         buckets[i] = ks->seq;
-        std::vector<uint64_t> kmers = extract_kmers_destructive(&buckets[i], 31);
+        filled_buckets.enqueue(i);
         ++count;
     }
 
+    done.store(true);
+    for (std::thread &w : workers)
+        w.join();
+
     std::fprintf(stderr, "Total reads: %zu\n", count);
 
-    for (int i = 0; i < 128; i++)
-        free(buckets[i].s);
+    for (kstring_t &bucket : buckets)
+        free(bucket.s);
     ks->seq.s = NULL; // pointer has already been freed
     kseq_destroy(ks);
     gzclose(fp);
     return 0;
 }
-
-// int main(int argc, char **argv)
-// {
-//     if (argc < 2)
-//     {
-//         std::fprintf(stderr, "Usage: %s <in.fastq.gz|in.fastq|->\n", argv[0]);
-//         return 1;
-//     }
-
-//     const char *path = argv[1];
-//     gzFile fp = std::strcmp(path, "-") ? gzopen(path, "r") : gzdopen(fileno(stdin), "r");
-//     if (!fp)
-//         return 1;
-
-//     kseq_t *seq = kseq_init(fp);
-//     size_t count = 0;
-//     while (kseq_read(seq) > -1)
-//     {
-//         ++count;
-//         // std::fprintf(stderr, "%zu\n", count);
-//         std::vector<uint64_t> kmers = extract_kmers_destructive(seq, 31);
-//     }
-
-//     std::fprintf(stderr, "Total reads: %zu\n", count);
-
-//     kseq_destroy(seq);
-//     gzclose(fp);
-//     return 0;
-// }
